4fa74da3df23e9769de46d61419a046d32767cf7
marciomartins12 <mmarciom1martins635@gmail.com>
2025-06-10 16:27:37 -0300

    cln

--- routes/routesPages.js
+++ routes/routesPages.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const pageController = require('../controllers/pageController');
-const userMiddleware = require("../middleware/userMiddleware");
-
 
 router.get('/', (req, res)=> {
     res.send("init")
