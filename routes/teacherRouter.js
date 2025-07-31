5e244d2a1d570fe3f271c01828f951c2e4c40d30
marciomartins12 <mmarciom1martins635@gmail.com>
2025-07-31 17:47:12 -0300

    --files

--- routes/teacherRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const teacherController = require("../controllers/teacherControlles");
-
-router.get("/", teacherController.dashboard);
-router.get("/classes", teacherController.classes);
-router.get("/grades", teacherController.grades);
-module.exports = router;
